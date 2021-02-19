53e425bbfcbd1ca17c01c385d5df5b6e75662a0b
Balazs Juhasz <juhaszbal@gmail.com>
2021-02-19 20:44:31 +0100

    add weather app code

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const dotenv = require('dotenv').config();
+
+const forecast = (latitude, longitude, callback) => {
+  const access_key = process.env.FORECAST_ACCESS_KEY;
+  const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=${latitude},${longitude}&units=m`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
